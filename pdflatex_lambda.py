8c5c39d9c12526aeb009f2270099032aac2c5fac
EC2 Default User <ec2-user@ip-172-31-50-239.ec2.internal>
2016-09-26 23:04:10 +0000

    Add python installer function for texlive-scheme-basic and rename file to make more sense

--- pdflatex_lambda.py
+++ pdflatex_lambda.py
@@ -1,4 +1,5 @@
 import sys
+import yum
 
 
 def pdflatex(*args):
@@ -7,3 +8,12 @@ def pdflatex(*args):
 
 def lambda_handler(event, context):
     pass
+
+
+def install_texlive_scheme_basic():
+    yb = yum.YumBase()
+    kwarg = {'name' : 'texlive-scheme-basic'}
+    yb.install(**kwarg)
+    yb.resolveDeps()
+    yb.buildTransaction()
+    yb.processTransaction()
