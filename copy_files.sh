8166fc4a4c82544f4e98b58b81667ee71e7c3dbc
EC2 Default User <ec2-user@ip-172-31-50-239.ec2.internal>
2016-09-26 22:38:42 +0000

    Go different direction

--- copy_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-while read name
-do
-	cp $name ./texlive-scheme-basic
-done < tlsb_files_with_sub_files.txt
-
--- get_files_recursively.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-while read name
-do
-	find $name -type f
-done < tlsb_files.txt
-
