b4baa4d09a8ea14e4858c445b0b67331dc134e71
Jesse Nayak <jdn4ae@virginia.edu>
2016-10-03 16:35:20 -0400

    Remove packages as we can just use the TexLive text gui installer

--- tlsb-packages-extracted/unpack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FILES=/home/ec2-user/pdflatex-lambda/tlsb-packages/*
-for f in $FILES
-do
-  echo "Processing $f"
-  # take action on each file. $f store current file name
-  rpm2cpio $f | cpio -idmv
-done
