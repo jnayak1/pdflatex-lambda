73503150efc6f5e977d753d82b53b25a1ceb91f9
Jesse Nayak <jdn4ae@virginia.edu>
2018-05-05 09:35:41 -0400

    renewed interest

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# pdflatex-lambda
+# pdflatex-lambda (WIP)
 AWS Lambda function handler for converting LaTeX documents into PDFs. Python.
 
 ### Creating TeXLive Package
@@ -7,8 +7,3 @@ AWS Lambda function handler for converting LaTeX documents into PDFs. Python.
 - `tar -xvzf install-tl-...`
 - `cd install-tl-...`
 - `install-tl -gui text`
-
-### Using TeXLive Package
-- Add /path/to/package/bin/x86_64-linux to PATH environment variable
-
-More info on the wiki and in issues (open and closed)
--- pdflatex_lambda.py
+++ pdflatex_lambda.py
@@ -1,36 +1,64 @@
 from __future__ import print_function
 
-import json
-import urllib
 import boto3
 import os
+import uuid
+import subprocess
+
 
 s3 = boto3.client('s3')
 
-
-def pdflatex(*args):
-    pass
-
-
-def setup_tlsb():
-    os.environ["PATH"] = os.environ["PATH"] + os.path.join(os.path.dirname(os.path.abspath(__file__)), '/tlsb-gui-installer/bin/x86_64-linux')
-    print(os.environ["PATH"])
-
-
 def lambda_handler(event, context):
-    setup_tlsb()
-    #print("Received event: " + json.dumps(event, indent=2))
-
-    # Get the object from the event and show its content type
+    
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
-    try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print(response.__dict__)
-        return response['ContentType']
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
+    key = event['Records'][0]['s3']['object']['key']
+    download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
+    s3.download_file(bucket, key, download_path)
 
-setup_tlsb()
+    my_env = os.environ.copy()
+    bashCommand = "./tlsb-gui-installer/bin/x86_64-linux/pdflatex" + download_path
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, env=my_env)
+    output, error = process.communicate()
+
+
+if __name__ == "__main__":
+  event = {  
+   "Records":[  
+      {  
+        "eventVersion":"2.0",
+        "eventSource":"aws:s3",
+        "awsRegion":"us-west-2",
+        "eventTime":"1970-01-01T00:00:00.000Z",
+        "eventName":"ObjectCreated:Put",
+        "userIdentity":{  
+          "principalId":"AIDAJDPLRKLG7UEXAMPLE"
+        },
+        "requestParameters":{  
+          "sourceIPAddress":"127.0.0.1"
+        },
+        "responseElements":{  
+          "x-amz-request-id":"C3D13FE58DE4C810",
+          "x-amz-id-2":"FMyUVURIY8/IgAtTv8xRjskZQpcIZ9KG4V5Wp6S7S/JRWeUWerMUE5JgHvANOjpD"
+        },
+        "s3":{  
+          "s3SchemaVersion":"1.0",
+          "configurationId":"testConfigRule",
+          "bucket":{  
+            "name": "latex-lambda",
+            "ownerIdentity":{  
+              "principalId":"A3NL1KOZZKExample"
+            },
+            "arn": ""
+          },
+          "object":{  
+            "key":"simple.tex",
+            "size":1024,
+            "eTag":"d41d8cd98f00b204e9800998ecf8427e",
+            "versionId":"096fKKXTRTtl3on89fVO.nfljtsv6qko"
+          }
+        }
+      }
+    ]
+  }
+  context = None
+  lambda_handler(event, context)
